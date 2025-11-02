7c6e38b889d42f36e68030c976d8b6a9507fef03
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-11-02 12:18:16 +0100

    Test all user crud functions

--- backend/tests/crud/test_user_crud.py
+++ backend/tests/crud/test_user_crud.py
@@ -1,18 +1,50 @@
 import uuid
 
-from sqlmodel import Session
 from ..utils.utils import random_email, random_lower_string, random_string
+from ..utils.utils import email,password,userCreate
 from app.logic.user_crud import *
 from app.models.survey import Survey
 from app.models.user import User, UserCreate
+from app.core.password_utils import verify_password
 
 def test_create_user(session):
-    email = random_email()
-    password = random_string()
-    userCreate = UserCreate(email=email, password=password)
     user = create_user(session, userCreate)
 
     assert user.email==email
+    assert verify_password(password, user.hashed_password)
+    assert user.role=="user"
     assert hasattr(user,"id")
+    assert hasattr(user,"created_at")
     assert isinstance(user.id, uuid.UUID)
+    assert user.is_active
+
+
+def test_get_user_by_id(session):
+    user = get_user_by_email(session, email)
+
+    assert isinstance(user, User)
+    assert user.email==email
+
+
+def test_get_user_by_id_return_none(session):
+    user = get_user_by_email(session, random_email())
+
+    assert user == None
+
+
+def test_get_users(session):
+    users = get_users(session)
+
+    assert isinstance(users, list)
+    assert users[0].email == email
+    assert len(users) == 1
+
+
+def test_delete_user(session):
+    user = session.exec(select(User).where(User.email == email)).one()
+    delete_user(session, user)
+
+    result = session.exec(select(User).where(User.email == email)).all()
+
+    assert result == []
 
--- backend/tests/utils/utils.py
+++ backend/tests/utils/utils.py
@@ -1,5 +1,8 @@
 import random
 import string
+from app.models.user import User, UserCreate
+from datetime import datetime, timezone
+from app.core.password_utils import hash_password
 
 def random_lower_string() -> str:
     return "".join(random.choices(string.ascii_lowercase, k=32))
@@ -8,5 +11,11 @@ def random_lower_string() -> str:
 def random_string() -> str:
     return "".join(random.choices(string.ascii_letters, k=32))
 
+
 def random_email() -> str:
     return f"{random_lower_string()}@{random_lower_string()}.com" 
+
+
+email = random_email()
+password = random_string()
+userCreate = UserCreate(email=email, password=password)
