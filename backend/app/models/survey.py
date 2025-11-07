c54f7c7fe7cf122467cc43d9b16469967a7f1747
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-11-07 19:08:55 +0100

    Enables none value for last_updated

--- backend/app/models/survey.py
+++ backend/app/models/survey.py
@@ -6,7 +6,6 @@ from .user import User
 
 class SurveyBase(SQLModel):
     name: str = Field(max_length=255)
-    is_anonymous: str = Field()
     status: str = Field(default="inactive")
 
 
@@ -18,7 +17,7 @@ class Survey(SurveyBase, table = True):
     id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
     created_at: datetime
     expires_at: datetime
-    last_updated: datetime
+    last_updated: datetime | None
 
     user_id: uuid.UUID = Field(foreign_key="user.id")
     user: User = Relationship(back_populates="surveys")  
