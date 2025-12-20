5dce567755f250f2f7563881913e1f03926c3fff
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2025-12-20 13:08:40 +0100

    Some small changes

--- backend/app/logic/policies.py
+++ backend/app/logic/policies.py
@@ -6,12 +6,9 @@ from ..models.user import User
 from ..core.exceptions import NotFoundError, NotOwnerError
 from .crud.survey_crud import get_survey_by_id
 import uuid
+from ..core.transaction import transactional
 
-
+@transactional()
 def survey_owner_required(*, session: Session = Depends(get_session), user: User = Depends(get_current_user), survey_id: uuid.UUID):
     survey = get_survey_by_id(session, survey_id)
-    if survey is None:
-        raise NotFoundError("Survey not found")
-    if survey.user_id != user.id:
-        raise NotOwnerError("You are not the owner of this survey")
     return survey
