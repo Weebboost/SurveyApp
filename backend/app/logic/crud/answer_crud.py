aeecdaadac668a936ec5f6b2d41fa35fe501ba7a
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2026-01-03 10:56:31 +0100

    Move files

--- backend/app/logic/crud/answer_crud.py
+++
@@ -1,6 +0,0 @@
-from ...models.answer import Answer
-from sqlmodel import Session
-
-def create_answer(*, session: Session, answer: Answer) -> Answer:
-    session.add(answer)
-    return answer
--- backend/app/logic/crud/choice_crud.py
+++
@@ -1,8 +0,0 @@
-import uuid
-
-from ...models.choice import Choice
-from sqlmodel import Session
-
-def create_choice(session: Session, choice: Choice) -> Choice:
-    session.add(choice)
-    return choice
