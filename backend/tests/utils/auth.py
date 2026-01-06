0d4b20e058bce0a5e97298cbf564c0403eb8eb16
AgnieszkaNeu <agnieszka.neumann13@gmail.com>
2026-01-06 10:33:18 +0100

    Remove unused test files and utility functions

--- backend/tests/utils/auth.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-
-def user_authentication_header(*, client: TestClient, email: str, password: str) -> dict[str:str]:
-    data = {"username": email, "password": password}
-    r = client.post("/login/token", data=data)
-    response = r.json()
-    auth_token = response["access_token"]
-    headers = {"Authorization": f"Bearer {auth_token}"}
-    return headers
